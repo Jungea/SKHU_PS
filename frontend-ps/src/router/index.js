800a00588d7f8737a22760ca06bd205319d2493c
YYLee26088 <26088@naver.com>
2020-04-21 13:59:07 +0900

    로그인 페이지 추가

--- frontend-ps/src/router/index.js
+++ frontend-ps/src/router/index.js
@@ -17,6 +17,15 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+
+  {
+    path: '/login',
+    name: 'form1',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
   }
 ]
 
