421a41ac547101f6e241e566897fac75be6160bf
Lee, youngsun <dlsnsiti@gmail.com>
2020-04-23 22:50:41 +0900

    signup 페이지 디자인 수정

--- frontend-ps/src/views/SignUp.vue
+++ frontend-ps/src/views/SignUp.vue
@@ -1,42 +1,44 @@
 <template>
   <div class="sign-up">    
-    <div class="inputbox">
-      <center>
-        <h5>회원가입(학생)</h5>
-        <hr>
-        <b-form @submit="signUp">
-          <table>
-            <tr>
-              <b-form-group id="input-group-1" label="아이디(학번)" label-for="input-1">
-                <b-form-input id="input-1" v-model="form.id" type="text" required placeholder="아이디를 입력하세요."></b-form-input>
-              </b-form-group>
-            </tr>
-            <tr>
-              <b-form-group id="input-group-2" label="비밀번호" label-for="input-2">
-                <b-form-input id="input-2" type="password" v-model="form.pass1" required placeholder="비밀번호를 입력하세요."></b-form-input>
-              </b-form-group>
-            </tr>
-            <tr>
-              <b-form-group id="input-group-3" label="비밀번호 확인" label-for="input-3">
-                <b-form-input id="input-3" type="password" v-model="form.pass2" required placeholder="비밀번호를 한번 더 입력하세요."></b-form-input>
-              </b-form-group>
-            </tr>
-            <tr>
-              <b-form-group id="input-group-4" label="이름" lebel-for="input-4">
-                <b-form-input id="input-4" type="text" v-model="form.name" required placeholder="이름을 입력하세요."></b-form-input>
-              </b-form-group>
-            </tr>
-            <tr>
-              <b-form-group id="input-group-5" label="이메일" label-for="input-5">
-                <b-form-input id="input-5" type="email" v-model="form.email" required placeholder="이메일을 입력하세요."></b-form-input>
-              </b-form-group>
-            </tr>
-            <tr style="text-align: right">
-              <b-button type="submit" variant="primary">가입</b-button>
-            </tr>
-          </table>
-        </b-form>
-      </center>
+    <div class="inputbox" style="margin:100px auto; width:500px;">
+      <b-jumbotron style="background:none; text-align:left">
+        <center>
+          <h2 style="margin-left: 60px">SignUp</h2>
+          <hr style="border:2px solid black; width: 500px;">
+          <b-form @submit="signUp">
+            <table>
+              <tr>
+                <b-form-group id="input-group-1" label="아이디" label-for="input-1">
+                  <b-form-input id="input-1" v-model="form.id" type="text" required placeholder="Enter Id"></b-form-input>
+                </b-form-group>
+              </tr>
+              <tr>
+                <b-form-group id="input-group-2" label="비밀번호" label-for="input-2">
+                  <b-form-input id="input-2" type="password" v-model="form.pass1" required placeholder="Enter Password"></b-form-input>
+                </b-form-group>
+              </tr>
+              <tr>
+                <b-form-group id="input-group-3" label="비밀번호 확인" label-for="input-3">
+                  <b-form-input id="input-3" type="password" v-model="form.pass2" required placeholder="Enter Password"></b-form-input>
+                </b-form-group>
+              </tr>
+              <tr>
+                <b-form-group id="input-group-4" label="이름" lebel-for="input-4">
+                  <b-form-input id="input-4" type="text" v-model="form.name" required placeholder="Enter Name"></b-form-input>
+                </b-form-group>
+              </tr>
+              <tr>
+                <b-form-group id="input-group-5" label="이메일" label-for="input-5">
+                  <b-form-input id="input-5" type="email" v-model="form.email" required placeholder="Enter Email"></b-form-input>
+                </b-form-group>
+              </tr>
+              <tr style="text-align: right">
+                <b-button type="submit">가입</b-button>
+              </tr>
+            </table>
+          </b-form>
+        </center>
+      </b-jumbotron>
     </div>
   </div>
 
@@ -78,6 +80,6 @@
 <style scoped>
   .sign-up { margin-top: 50px ;}
   .inputbox { padding: 0px auto ;}
-  input { width: 500px; background-color: rgb(235,235,235); }
+  input { width: 500px; }
   button { margin-top: 10px; width: 80px; cursor: pointer; } 
 </style>
