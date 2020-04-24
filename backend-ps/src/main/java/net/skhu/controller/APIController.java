47b3e412f35f703a96984faf8fb7cbb20c44ac3f
Jungea <jungea2241@gmail.com>
2020-04-24 17:16:30 +0900

    AWS mysql 연결 확인

---
+++ backend-ps/src/main/java/net/skhu/controller/APIController.java
@@ -0,0 +1,23 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.skhu.domain.Common;
+import net.skhu.repository.CommonRepository;
+
+@RestController
+@RequestMapping("api")
+public class APIController {
+	@Autowired
+	CommonRepository commonRepository;
+	
+	@RequestMapping("commons")
+	public List<Common> commons() {
+		return commonRepository.findAll();
+	}
+
+}
