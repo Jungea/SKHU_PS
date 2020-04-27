a2fd534f8bc1cbe4cc05576583e7692511822394
Lee, youngsun <dlsnsiti@gmail.com>
2020-04-28 01:39:44 +0900

    `#11 CompoMyProjectList 즐겨찾기 버튼 추가

--- frontend-ps/src/components/CompoMyProjectList.vue
+++ frontend-ps/src/components/CompoMyProjectList.vue
@@ -12,10 +12,19 @@
                 </b-row>    
                 <b-row cols="2">    
                     <b-col :key="item" v-for="item in data">
-                    <b-card @click="viewSummary" align="left" bg-variant="dark" :header="item.projectName" text-variant="white" style="width: 25rem; height: 10rem; margin: 60px;">    
-                        
-                        <b-card-text>   
-                            {{item.content}}   
+                    <b-card @click="viewSummary" align="left" bg-variant="dark"  text-variant="white" style="width: 30rem; height: 15rem; margin: 60px;"> <!-- 30rem == 480px -->
+                        <div>
+                            <b-card-header style="padding: 0 0 10px 0">
+                                <table>
+                                    <tr>
+                                        <td style="width: 100%">{{item.projectName}}</td>
+                                        <td><b-button @click="pin1" style="background-color: rgb(52,58,64) ; border-color: rgb(52,58,64) ; margin-top: -10px" ><b-icon scale=1.5 v-bind:icon="starIcon.name"></b-icon></b-button></td>
+                                    </tr>
+                                </table>
+                            </b-card-header>
+                       </div>
+                        <b-card-text style="margin-top: 10px">
+                            {{item.content}}
                         </b-card-text>    
                     </b-card>    
                     </b-col>
@@ -90,7 +99,9 @@ export default {
         return {
             fields: { 프로젝트이름: "", 사용언어 : "",사용기술:"",주제:"",내용:"" },
             items: [{ 프로젝트이름: "", 사용언어:"" ,사용기술:"",주제:"",내용:""}],
+            items2: [{'플젝 이름': '파이썬 프로젝트', '사용 언어': '자바','사용 도구':'스프링','주제':'자바로 계산기 만들기','내용':'자바를 이용해서 배운걸 토대로 만들어볼 예정입니다.','모집여부':'모집중'},               ],
             data:{},
+            starIcon: { name: 'star', pin: false},
             
             // 프로젝트 생성 모달 창
             rcrtState:false,
@@ -110,12 +121,32 @@ export default {
     },
 
      methods: {
-        viewSummary() {
-            this.$router.push({
-                path: '/summary'
-                })
-            }
-        },
+                viewSummary() {                    
+                    this.$router.push({
+                    path: '/summary'
+                    })
+                },
+                pin1() {                    
+                    if(this.starIcon.name == "star")
+                    {
+                        this.starIcon.name = "star-fill"
+                        this.starIcon.pin = true
+                    }
+                    else
+                    {
+                        this.starIcon.name = "star"
+                        this.starIcon.pin = false
+                    }
+
+                    alert(this.starIcon.pin)
+
+                    axios.post('url', {
+                        pin: this.starIcon.pin
+                    })
+                    .then()
+                    
+                    event.stopPropagation()
+                },
         handleSubmit() {
             // Exit when the form isn't valid
             if (!this.checkFormValidity()) {
@@ -172,6 +203,7 @@ export default {
             }
         },
     }
+}
 
 </script>
 
