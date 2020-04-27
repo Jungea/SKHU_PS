b0cda6edac2f4010d2bc7a2932a63738bd3109db
bom04 <gil1122@naver.com>
2020-04-27 21:22:30 +0900

    #11 프로젝트 생성 모달 수정

--- frontend-ps/src/components/CompoMyProjectList.vue
+++ frontend-ps/src/components/CompoMyProjectList.vue
@@ -18,31 +18,52 @@
                             {{item.content}}   
                         </b-card-text>    
                     </b-card>    
-
                     </b-col>
                 </b-row>    
             </b-container>    
         </center>    
     
         <!--프로젝트 생성 모달 -->
-        <b-modal id="modal-xl" size="lg" title="프로젝트 생성">
-            <b-table stacked :items="items" :fields="fields">
-            <template v-slot:cell(프로젝트이름)="row">
-                <b-form-input v-model="row.item.프로젝트이름"/>
-            </template>
-            <template v-slot:cell(사용언어)="row">
-                <b-form-input v-model="row.item.사용언어"/>
-            </template>
-            <template v-slot:cell(사용기술)="row">
-                <b-form-input v-model="row.item.사용기술"/>
-            </template>
-            <template v-slot:cell(주제)="row">
-                <b-form-input v-model="row.item.주제"/>
-            </template>
-            <template v-slot:cell(내용)="row">
-                <b-form-textarea v-model="row.item.내용"/>
-            </template>
-        </b-table>
+        <b-modal id="modal-xl" size="lg" title="프로젝트 생성" @ok="submit">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th scope="row" style="width:28%">프로젝트명</th>
+                        <td><b-form-input  :state="projectName.length <= 10 && projectName.length>0" v-model="projectName" id="input-default" placeholder="프로젝트 이름을 입력하세요"></b-form-input></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">주제</th>
+                        <td><b-form-input id="input-default" v-model="theme" placeholder="주제를 입력하세요"></b-form-input></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">내용</th>
+                        <td><b-form-textarea id="textarea-state" v-model="content" placeholder="내용을 입력하세요"></b-form-textarea></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">사용기술과 언어</th>
+                        <td>
+                            <b-form-tags
+                                input-id="tags-remove-on-delete"
+                                :input-attrs="{ 'aria-describedby': 'tags-remove-on-delete-help' }"
+                                v-model="tag"
+                                separator=" ,;"
+                                placeholder="태그를 입력하세요"
+                                remove-on-delete
+                                add-on-enter
+                                class="mb-2"
+                            ></b-form-tags>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">인원 모집 상태</th>
+                        <td><b-form-select v-model="rcrtState" :options="options"></b-form-select></td>
+                    <tr>
+                        <th scope="row">과목 인증키 </th>
+                        <td><b-form-input id="input-default" v-model="authKey" placeholder="수강 과목의 인증키를 입력하세요"></b-form-input></td>
+                    </tr>
+                </tbody>
+ 
+            </table>
         </b-modal>
     </div>
 
@@ -62,9 +83,30 @@ export default {
         return {
             fields: { 프로젝트이름: "", 사용언어 : "",사용기술:"",주제:"",내용:"" },
             items: [{ 프로젝트이름: "", 사용언어:"" ,사용기술:"",주제:"",내용:""}],
-            items2: [{'플젝 이름': '파이썬 프로젝트', '사용 언어': '자바','사용 도구':'스프링','주제':'자바로 계산기 만들기','내용':'자바를 이용해서 배운걸 토대로 만들어볼 예정입니다.','모집여부':'모집중'},               ],
-            data:{}
+            data:{},
+            
+            // 프로젝트 생성 모달 창
+            rcrtState:false,
+            projectName:'',
+            theme:'',
+            content:'',
+            options: [
+                { value: false, text: '모집중' },
+                { value: true, text: '모집 완료' },
+            ],
+            tag:[],
+            authKey:'',
         };
+    },
+    methods: {
+        submit() {
+            axios.post('/api/makeProject'),{
+
+            }.then(response => { 
+            this.user = response.data;
+                
+            });
+        }
     }
 }
 </script>
