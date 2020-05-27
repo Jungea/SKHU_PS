769d16662b3a0fc06dabbc1293fa82534bcedfca
Jungea <jungea2241@gmail.com>
2020-05-27 20:14:46 +0900

    #20 프로젝트 정보 팀원추가

--- frontend-ps/src/components/CompoProjectSummary.vue
+++ frontend-ps/src/components/CompoProjectSummary.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="containerStyle">
-        <h3>프로젝트 개요</h3>
+        <h3>프로젝트 정보</h3>
         <hr>        
         <form ref="form">
             <b-form-group label-for="ProjectSummary">
@@ -12,16 +12,22 @@
                                 {{ this.data.projectName }}
                             </td>
                         </tr>
-                         <tr>
+                        <tr>
                             <th scope="row">멤버 수</th>
                             <td>
                                 {{ this.data.memNum }}
                             </td>
                         </tr>
                         <tr>
+                            <th scope="row">팀장</th>
+                            <td>
+                                {{ this.data.user.name }}({{this.data.user.grade}})
+                            </td>
+                        </tr>
+                        <tr>
                             <th scope="row">팀원</th>
                             <td>
-                                {{ this.memList.toString() }}
+                                {{ this.memList.join(", ") }}
                             </td>
                         </tr>
                         <tr>
@@ -80,45 +86,56 @@
 <script>
 import axios from 'axios';
 export default {
-    watch: {
-        '$route' () {
-            axios.get('/api/project/'+this.$route.params.projectId)
-            .then(response => {
-                this.data = response.data
-                if(this.data.tag)
-                    this.tagArray=this.data.tag.split(',')
-                if(this.data.subject == null)
-                    this.subject = '과목없음'
-                else
-                    this.subject = this.data.subject.title
-            })
+    data() {
+        return { 
+            data: {},
+            subject: '',
+            state: '',
+            progState: '',
+            tagArray:[],
+            memList: []
         }
     },
-    mounted() {
-        axios.get('/api/project/'+this.$route.params.projectId)
-        .then(response => {
-            this.data = response.data
-            if(this.data.tag)
-                this.tagArray=this.data.tag.split(',')
-            if(this.data.subject == null)
-                this.subject = '과목없음'
-            else
-                this.subject = this.data.subject.title
-            this.rcrtStateCheck()
-            this.progStateCheck()
-            
-            axios.get('/api/project/' + this.$route.params.projectId + '/member')
-            .then(response => {
-                this.memList.push(this.data.user.name)
-                if(response.data.length != 0) {
-                    for(var i = 0 ; i < response.data.length ; i++) {
-                        this.memList.push(' ' + response.data[i].user.name);
-                    }
-                }
-            })
-        })
+
+    // url 파라미터 변화가 일어나는 것을 인지(사이드바 pin 선택 시) 
+    watch: {
+        '$route' () {
+            this.reload();
+        }
     },
+
+    mounted() {
+        this.reload();
+    },
+
     methods: {
+        reload() {
+            // 프로젝트
+            axios.get('/api/project/'+this.$route.params.projectId)
+                .then(response => {
+                    this.data = response.data;
+                    if(this.data.tag)
+                        this.tagArray = this.data.tag.split(',');
+                    if(this.data.subject == null)
+                        this.subject = '과목없음'
+                    else
+                        this.subject = this.data.subject.title;
+                    this.rcrtStateCheck();
+                    this.progStateCheck(); 
+                })
+
+            // 팀원
+            axios.get('/api/project/' + this.$route.params.projectId + '/member')
+                .then(response => {
+                    this.memList = [];
+                    if(response.data.length != 0) {
+                        for(var i = 0 ; i < response.data.length ; i++) {
+                            this.memList.push(response.data[i].user.name+'('+response.data[i].user.grade+')');
+                        }
+                    }
+                })
+        },
+        
         rcrtStateCheck() {
            if(this.data.rcrtState == 0)
                 this.state = '모집중'
@@ -137,16 +154,7 @@ export default {
             })
         }
     },
-    data() {
-        return { 
-            data: {},
-            subject: '',
-            state: '',
-            progState: '',
-            tagArray:[],
-            memList: []
-        }
-    }
+    
 }
 </script>
 
